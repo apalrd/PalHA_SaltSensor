0f130c8b3978c3b6769f519b3d6a87032869cda5
Andrew Palardy <apalrd@gmail.com>
2021-04-30 15:51:50 -0400

    Got everything functional

--- src/main.c
+++ src/main.c
@@ -14,14 +14,69 @@
 #include "freertos/task.h"
 #include "esp_log.h"
 #include "maxbotix.h"
+#include "ethernet.h"
+#include "mqtt.h"
 
 static const char *TAG = "main";
+
+
+static void app_task(void *pvParameters)
+{
+    /* Generate topic ID from clid */
+    static char topic[128];
+    static char element[128];
+    static char msg[1024];
+    sprintf(topic,"raw/%s/ultra",mqtt_clid);
+    ESP_LOGI(TAG,"Publish topic is %s",topic);
+
+    while(1)
+    {
+        /* Publish to MQTT every 1 minute */
+        int16_t count = 0;
+        uint16_t sample = maxbotix_get_latest();
+        int32_t age = maxbotix_get_age();
+        float result = maxbotix_get_median(0.6f,8,64,&count);
+        ESP_LOGI(TAG,"Median sample returned %f, sample count %d",(double)result,count);
+        
+        /* Start with curly brace */
+        strcpy(msg,"{");
+        /* Always add latest reading */
+        sprintf(element,"\"Latest\": %d,",sample);
+        strcat(msg,element);
+
+        /* Always add age */
+        sprintf(element,"\"Age\": %d",age);
+        strcat(msg,element);
+
+        /* Include result and count if not negative */
+        if(result >= 0)
+        {
+            sprintf(element,", \"Dist\": %f,",result);
+            strcat(msg,element);
+            sprintf(element," \"Count\": %d",count);
+            strcat(msg,element);
+        }
+        /* Add trailing curly brace */
+        strcat(msg,"}");
+        ESP_LOGI(TAG,"Publish topic is %s",topic);
+        ESP_LOGI(TAG,"Payload to publish is %s",msg);
+
+        /* Actually publish the message */
+        int id = esp_mqtt_client_publish(mqtt_client, topic, msg, 0, 0, 0);
+        ESP_LOGI(TAG,"Published mesage with ID %d",id);
+
+        /* Wait delay for 1 minute interval */
+        vTaskDelay(pdMS_TO_TICKS(1000*60));
+    }
+}
 
 void app_main(void)
 {
     /* Call Ethernet function */
-    void eth_begin();
-    eth_begin();
+    eth_init();
+
+    /* Call MQTT function */
+    mqtt_init();
 
     /* Call Maxbotix function */
     maxbotix_init();
@@ -29,14 +84,6 @@ void app_main(void)
     /* Reduce logging from maxbotix */
     esp_log_level_set("maxbotix", ESP_LOG_WARN);
 
-
-    while(1)
-    {
-        uint16_t sample = maxbotix_get_latest();
-        int16_t count = 0;
-        ESP_LOGI(TAG,"Continuing to do things, latest sample is %d",sample);
-        float result = maxbotix_get_median(0.6f,8,64,&count);
-        ESP_LOGI(TAG,"Median sample returned %f, sample count %d",(double)result,count);
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
+    /* Start main task at lower priority */
+    xTaskCreatePinnedToCore(app_task, "app_task", 2048, NULL, 1, NULL,1);
 }
